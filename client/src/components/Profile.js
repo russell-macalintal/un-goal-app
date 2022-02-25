e0e7d5dccfeb3fb4423c4c2685d032d18cf66b53
Cassidy Lim <cassidylim7@gmail.com>
2022-02-25 13:06:58 -0500

    Added bootstrap and static data for profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import volunteer from '../Data/volunteerData'; 
 
 export default function Profile() {
+
+    useEffect(() => {
+
+    }, [])
+
   return (
-    <div>Profile</div>
+    <div>
+        <h3>Volunteer Name</h3>
+        <h4>Volunteer Email</h4>
+
+        <h3>Availability</h3>
+        
+
+        <h4>Reward Panel</h4>
+
+    </div>
   )
 }
