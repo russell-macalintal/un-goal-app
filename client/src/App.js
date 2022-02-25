5c4fbd1452959ac719659ba12c61f2c00af58aa2
Russell M <russell.macalintal@gmail.com>
2022-02-25 12:06:06 -0500

    Added basic routes and Home.js functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-  
+      <Router>
+        <>
+          <Route exact path="/" component={Home} />
+        </>
+      </Router>
     </div>
   );
 }
