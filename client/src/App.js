f47adee1b31730771c4ca1fb9cfd130f5d6edd5e
Russell M <russell.macalintal@gmail.com>
2022-02-25 11:15:22 -0500

    Added components folder and default imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
