2887cc6f2cad4bcd66915ada2f4bff14f2a4b3e7
Cassidy Lim <cassidylim7@gmail.com>
2022-02-25 12:20:06 -0500

    Added frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import NavBar from './components/NavBar';
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <>
-          <Route exact path="/" component={Home} />
-        </>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
       </Router>
     </div>
   );
