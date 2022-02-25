22fd968e3599d88d8837529bdb6e6459e6640b44
Cassidy Lim <cassidylim7@gmail.com>
2022-02-25 14:05:43 -0500

    Added calender component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import Calender from './components/Calender';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/calender" element={<Calender />} />
         </Routes>
       </Router>
     </div>
