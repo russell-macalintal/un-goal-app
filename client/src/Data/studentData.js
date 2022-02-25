49d63be5a0c92b94bd516c55565031079288b5ac
mligon115 <mligon115@yahoo.com>
2022-02-25 11:34:06 -0600

    added dummy data

---
+++ client/src/Data/studentData.js
@@ -0,0 +1,33 @@
+const students = [
+  {
+    id: 1,
+    name: 'susan smith',
+    subject: 'web developer',
+    location:'New York, NY',
+    time: '5 pm',
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    subject: 'web designer',
+    location:'New York, NY',
+    time: '5 pm',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    subject: 'intern',
+    location:'New York, NY',
+    time: '5 pm',
+  },
+  {
+    id: 4,
+    name: 'Kay Anderson',
+    subject: 'spelling',
+    location: 'New York, NY',
+    time: '5 pm'
+
+  },
+];
+
+export default students
